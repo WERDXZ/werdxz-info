182c92112b106255227796683e3bc12633aa94c5
WERDXZ <lwerdxzl@hotmail.com>
2024-02-14 00:16:11 -0800

    change build script:P

--- css/build.rs
+++ css/build.rs
@@ -6,13 +6,13 @@ use std::env;
 fn main() {
     println!("excecuting buildscript");
     env::set_current_dir("./css").expect("Failed to change directory");
-    tailwindcss_build("./global.css", "main.css");
+    tailwindcss_build("./global.css", "main.generated.css");
 }
 
 fn tailwindcss_build(source: &str, output: &str) {
     Command::new("tailwindcss")
         .args(&["-i", source])
-        .args(&["-o", &format!("../../static/assets/style/{output}")])
+        .args(&["-o", &format!("{}/static/assets/style/{output}",env!("CARGO_MANIFEST_DIR"))])
         // .args(&["-c", TAILWINDCSS_CONFIG])
         .arg("--minify")
         .status()
